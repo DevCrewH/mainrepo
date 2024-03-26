e797e2a8dc689ff31521a98bfc444513d63ba279
beimnet melese <beimnetmelese16@gmail.com>
2024-03-27 00:17:43 +0300

    user register and login feature created

--- OnlineMarket/OnlineMarket/settings.py
+++ OnlineMarket/OnlineMarket/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'djoser',
     'user',
 ]
 AUTH_USER_MODEL = 'user.User'
@@ -122,3 +124,17 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DJOSER = {
+    "SERIALIZERS": {
+        "user_create": "user.serializer.UserCreationSerializer",
+    }
+}
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
+SIMPLE_JWT = {
+   'AUTH_HEADER_TYPES': ('JWT',),
+}
---
+++ OnlineMarket/user/serializer.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from .models import User
+from djoser.serializers import UserCreateSerializer
+
+class UserCreationSerializer(UserCreateSerializer):
+    class Meta(UserCreateSerializer.Meta):
+        fields = ["id","first_name", "last_name", "username", "email","sex","phone","password"]
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["first_name", "last_name", "email", "username", "sex", "phone", "password"]
+
